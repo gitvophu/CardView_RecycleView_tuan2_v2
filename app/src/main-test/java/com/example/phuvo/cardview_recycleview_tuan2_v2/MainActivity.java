d17abcba3aec7e1fc1760a6ed8b173af2dfa6c9c
Phu Vo <vongocphu04@gmail.com>
2019-02-25 23:32:57 +0700

    Initial commit

---
+++ app/src/main-test/java/com/example/phuvo/cardview_recycleview_tuan2_v2/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.phuvo.cardview_recycleview_tuan2_v2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
